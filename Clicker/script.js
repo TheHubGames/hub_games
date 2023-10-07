70469a16c4a67c73442bcef776ad69d67e5d2269
Klaynight <63601267+Klaynight-dev@users.noreply.github.com>
2023-10-07 22:28:38 +0200

    Update script.js
    Signed-off-by: Klaynight <63601267+Klaynight-dev@users.noreply.github.com>

--- Clicker/script.js
+++ Clicker/script.js
@@ -24,53 +24,53 @@ function returnToHub() {
         clickCount.textContent = `${clicks} clics`;
     });
 
-    function startCountdown() {
-        const countdownInterval = setInterval(() => {
-            countdown--;
-            countdownDisplay.textContent = `Temps restant: ${countdown} secondes`;
+function startCountdown() {
+    const countdownInterval = setInterval(() => {
+        countdown--;
+        countdownDisplay.textContent = `Temps restant: ${countdown} secondes`;
 
-            if (countdown === 0) {
-                clearInterval(countdownInterval);
-                // Afficher le résultat du nombre de clics divisé par 15
-                const result = (clicks / 15).toFixed(2);
-                cpsResult.textContent = `CPS: ${result}`;
-                cpsResult.style.display = "block"; // Rendre l'élément visible
+        if (countdown === 0) {
+            clearInterval(countdownInterval);
+            // Afficher le résultat du nombre de clics divisé par 15
+            const result = (clicks / 15).toFixed(2);
+            cpsResult.textContent = `CPS: ${result}`;
+            cpsResult.style.display = "block"; // Rendre l'élément visible
 
-                // Obtenir la date et l'heure actuelles au format "dd/mm/aaaa hh:mm"
-                const currentDate = new Date();
-                const formattedDate = `${currentDate.getDate().toString().padStart(2, '0')}/${(currentDate.getMonth() + 1).toString().padStart(2, '0')}/${currentDate.getFullYear()} ${currentDate.getHours().toString().padStart(2, '0')}:${currentDate.getMinutes().toString().padStart(2, '0')}`;
+            // Obtenir la date et l'heure actuelles au format "dd/mm/aaaa hh:mm"
+            const currentDate = new Date();
+            const formattedDate = `${currentDate.getDate().toString().padStart(2, '0')}/${(currentDate.getMonth() + 1).toString().padStart(2, '0')}/${currentDate.getFullYear()} ${currentDate.getHours().toString().padStart(2, '0')}:${currentDate.getMinutes().toString().padStart(2, '0')}`;
 
-                // Ajouter le résultat CPS et la date/heure à l'historique
-                const listItem = document.createElement("li");
-                listItem.textContent = `${formattedDate} - CPS: ${result}`;
-                histCPS.appendChild(listItem);
+            // Ajouter le résultat CPS et la date/heure à l'historique
+            const listItem = document.createElement("li");
+            listItem.textContent = `${formattedDate} - CPS: ${result}`;
+            histCPS.appendChild(listItem);
 
-                // Masquer le temps après les 15 secondes
-                countdownDisplay.style.display = "none";
+            // Masquer le temps après les 15 secondes
+            countdownDisplay.style.display = "none";
 
-                // Masquer le bouton "Cliquez ici!" après les 15 secondes
-                clickButton.style.display = "none";
+            // Masquer le bouton "Cliquez ici!" après les 15 secondes
+            clickButton.style.display = "none";
 
-                // Afficher le bouton "Refaire un test"
-                retryButton.style.display = "block";
-            }
-        }, 1000);
-    }
+            // Afficher le bouton "Refaire un test"
+            retryButton.style.display = "block";
+        }
+    }, 1000);
+}
 
 
-    retryButton.addEventListener("click", () => {
-        clicks = 0;
-        countdown = 15;
-        countdownStarted = false;
-        cpsResult.style.display = "none";
+retryButton.addEventListener("click", () => {
+    clicks = 0;
+    countdown = 15;
+    countdownStarted = false;
+    cpsResult.style.display = "none";
 
-        // Afficher à nouveau le temps lorsque vous cliquez sur "Refaire un test"
-        countdownDisplay.style.display = "block";
+    // Afficher à nouveau le temps lorsque vous cliquez sur "Refaire un test"
+    countdownDisplay.style.display = "block";
 
-        // Afficher à nouveau le bouton "Cliquez ici!" lorsque vous cliquez sur "Refaire un test"
-        clickButton.style.display = "block";
+    // Afficher à nouveau le bouton "Cliquez ici!" lorsque vous cliquez sur "Refaire un test"
+    clickButton.style.display = "block";
 
-        retryButton.style.display = "none";
-        clickCount.textContent = "0 clics";
-        countdownDisplay.textContent = "Attendez de cliquer...";
-    });
+    retryButton.style.display = "none";
+    clickCount.textContent = "0 clics";
+    countdownDisplay.textContent = "Attendez de cliquer...";
+});
