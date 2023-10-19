6805f983c9a8762985b98ebcbb4caadb8ebba7d8
Klaynight <63601267+Klaynight-dev@users.noreply.github.com>
2023-10-19 22:57:24 +0200

    Create script.js
    Signed-off-by: Klaynight <63601267+Klaynight-dev@users.noreply.github.com>

---
+++ MAJ/script.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function() {
+    // La page est entièrement chargée, masquez l'écran de chargement.
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").style.display = "block"; // Affiche le contenu principal
+    });
