07e8fdefd7a05b6927f4f98ea85c2d44f6dad39a
Klaynight <63601267+Klaynight-dev@users.noreply.github.com>
2023-10-19 17:49:36 +0200

    Add files via upload
    Signed-off-by: Klaynight <63601267+Klaynight-dev@users.noreply.github.com>

--- MAJ/All_maj/script.js
+++ MAJ/All_maj/script.js
@@ -1,33 +1,37 @@
-const repoOwner = 'TheHubGames'; // Remplacez par le nom du propriétaire du dépôt
-const repoName = 'hub_games'; // Remplacez par le nom du dépôt
+document.addEventListener("DOMContentLoaded", function () {
+    const repoOwner = 'TheHubGames'; // Remplacez par le nom du propriétaire du dépôt
+    const repoName = 'hub_games'; // Remplacez par le nom du dépôt
 
-const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/releases/latest`;
+    const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/releases`;
 
-fetch(apiUrl)
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Erreur lors de la récupération de la dernière release depuis GitHub');
-    }
-    return response.json();
-})
-.then(data => {
-    const releasesDiv = document.getElementById('github-releases');
-    const md = window.markdownit();
-    document.getElementById('load').style.display = 'none';
+    fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erreur lors de la récupération des releases depuis GitHub');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const releasesDiv = document.getElementById('github-releases');
+        const md = window.markdownit();
+        document.getElementById('load').style.display = 'none';
+        
+        data.forEach(release => {
+            const releaseContainer = document.createElement('div');
+            releaseContainer.classList.add('release-container');
 
-    const releaseContainer = document.createElement('div');
-    releaseContainer.classList.add('release-container');
+            const releaseHTML = `<div>
+                <h3>${release.name}</h3>
+                <p>${md.render(release.body)}</p>
+            </div>`;
+            releaseContainer.innerHTML = releaseHTML;
 
-    const releaseHTML = `<div>
-        <h3>${data.name}</h3>
-        <p>${md.render(data.body)}</p>
-    </div>`;
-    releaseContainer.innerHTML = releaseHTML;
-
-    releasesDiv.appendChild(releaseContainer);
-})
-.catch(error => {
-    const releasesDiv = document.getElementById('github-releases');
-    releasesDiv.innerHTML = '<p>Erreur lors de la récupération de la dernière release depuis GitHub</p>';
-    console.error('Erreur lors de la récupération de la dernière release depuis GitHub', error);
+            releasesDiv.appendChild(releaseContainer);
+        });
+    })
+    .catch(error => {
+        const releasesDiv = document.getElementById('github-releases');
+        releasesDiv.innerHTML = '<p>Erreur lors de la récupération des releases depuis GitHub</p>';
+        console.error('Erreur lors de la récupération des releases depuis GitHub', error);
+    });
 });
