f5a1145400f91a1980dd0a0e26fe27288ad0ddce
Klaynight <63601267+Klaynight-dev@users.noreply.github.com>
2023-10-19 11:45:18 +0200

    Update life.js
    Signed-off-by: Klaynight <63601267+Klaynight-dev@users.noreply.github.com>

--- Jeu_de_la_vie/js/life.js
+++ Jeu_de_la_vie/js/life.js
@@ -1172,3 +1172,9 @@ var life = function () {
 }();
 
 window.onload = life.load;
+
+window.addEventListener("load", function() {
+    // La page est entièrement chargée, masquez l'écran de chargement.
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").style.display = "block"; // Affiche le contenu principal
+});
