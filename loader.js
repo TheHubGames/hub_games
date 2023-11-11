4303a119421f06a2c0e805b64d35dcdaad79ee36
Klaynight <63601267+Klaynight-dev@users.noreply.github.com>
2023-11-11 20:04:08 +0100

    Patch Loader
    Signed-off-by: Klaynight <63601267+Klaynight-dev@users.noreply.github.com>

--- loader.js
+++ loader.js
@@ -1,5 +1,4 @@
-window.addEventListener("loader", function() {
+window.addEventListener("load", function() {
     // La page est entièrement chargée, masquez l'écran de chargement.
     document.getElementById("loader").style.display = "none";
-    document.getElementById("content").style.display = "block"; // Affiche le contenu principal
     });
