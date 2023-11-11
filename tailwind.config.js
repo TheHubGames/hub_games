f1b2f23df38d2663ce0434923031e4d9ead4e979
Klaynight <63601267+Klaynight-dev@users.noreply.github.com>
2023-11-11 14:45:53 +0100

    Add files via upload
    Signed-off-by: Klaynight <63601267+Klaynight-dev@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+module.exports = {
+    // ...
+    plugins: [
+      require('@tailwindcss/aspect-ratio'),
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/line-clamp'),
+      require('@tailwindcss/typography'),
+      // ... other plugins
+    ],
+  }
+  
